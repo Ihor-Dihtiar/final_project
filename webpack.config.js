337e60093fdd3523a9f894477da8921333e03bef
Igor Digtiar <ihordihtiar@gmail.com>
2020-09-05 17:28:31 +0300

    new webpack and file structure config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,3 @@
-// const path = require('path');
-
-// module.exports = {
-//   entry: './src/scripts/index.js',
-//   devtool: 'source-map',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'build/scripts'),
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         loader: 'babel-loader',
-//       },
-//     ],
-//   },
-// };
-
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 console.log(process.env.WEBPACK_MODE);
